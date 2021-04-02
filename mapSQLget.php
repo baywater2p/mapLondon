d25d76c4e36c4e810c60c4a75db9fd319fd86239
Bayswater2p <80769192+baywater2p@users.noreply.github.com>
2021-04-02 09:53:48 +0100

    Update mapSQLget.php

--- mapSQLget.php
+++ mapSQLget.php
@@ -4,17 +4,17 @@ $lon = $_GET['lon'];
 $lat2 = $_GET['lat2'];
 $lon2 = $_GET['lon2'];
 
-$con = mysqli_connect('ftp.domain.com','username','password','db');
+$con = mysqli_connect('db.2pence.co.uk','username','password','db');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
 mysqli_select_db($con,"db");
-$sql="SELECT * FROM stops WHERE stop_lat BETWEEN ".$lat." AND ".$lat2." AND stop_lon BETWEEN ".$lon." AND ". $lon2." LIMIT 40 " ;
+$sql="SELECT * FROM lockers WHERE stop_lat BETWEEN ".$lat." AND ".$lat2." AND stop_lon BETWEEN ".$lon." AND ". $lon2." LIMIT 40 " ;
 $result = mysqli_query($con,$sql);
    
 while($row = mysqli_fetch_array($result)) {
- echo "L.marker([" . $row['stop_lat'] . ", " .$row['stop_lon'] . "], {icon: busStop}).bindPopup('" .$row['stop_code'] ."# : ".$row['stop_name']  ."<br>Send <a href=sms:628246;?&body=BUS%20".$row['stop_code']."%20001>BUS stop# bus#</a> to 628246').addTo(cities);" ;
+ echo "L.marker([" . $row['locker_lat'] . ", " .$row['locker_lon'] . "], {icon: locker}).bindPopup(' " . $row['locker_addr'] ." ').addTo(cities);" ;
 }   
 mysqli_close($con);
 ?>
