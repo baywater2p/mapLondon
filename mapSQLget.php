d273eaf0ac1e7cd77b1dfd2c27447bbe998f4d9c
Bayswater2p <80769192+baywater2p@users.noreply.github.com>
2021-04-06 17:32:53 +0100

    Update mapSQLget.php

--- mapSQLget.php
+++ mapSQLget.php
@@ -4,17 +4,18 @@ $lon = $_GET['lon'];
 $lat2 = $_GET['lat2'];
 $lon2 = $_GET['lon2'];
 
-$con = mysqli_connect('db.2pence.co.uk','username','password','uk');
+$con = mysqli_connect('db.2pence.co.uk', 'user', 'pass', 'mainframe');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-mysqli_select_db($con,"uk");
-$sql="SELECT * FROM lockers WHERE locker_lat BETWEEN ".$lat." AND ".$lat2." AND locker_lon BETWEEN ".$lon." AND ". $lon2." LIMIT 40 " ;
+mysqli_select_db($con,"mainframe");
+$sql="SELECT * FROM locs WHERE lat BETWEEN ".$lat." AND ".$lat2." AND lon BETWEEN ".$lon." AND ". $lon2." LIMIT 40 " ;
 $result = mysqli_query($con,$sql);
    
 while($row = mysqli_fetch_array($result)) {
- echo "L.marker([" . $row['locker_lat'] . ", " .$row['locker_lon'] . "], {icon: locker}).bindPopup(' " . $row['locker_addr'] ." ').addTo(lockers);" ;
+ echo "L.marker([" . $row['lat'] . ", " .$row['lon'] . "], {icon: locker}).addTo(map);" ;
+
 }   
 mysqli_close($con);
 ?>
